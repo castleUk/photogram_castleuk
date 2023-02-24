fcdeb8da1bf5c37dcbcde0b35a0155ce09ce4b3d
unknown <ppkk34@gmail.com>
2023-02-24 18:11:01 +0900

    Subscribe

--- src/main/java/com/cos/photogramstart/service/SubscribeService.java
+++ src/main/java/com/cos/photogramstart/service/SubscribeService.java
@@ -1,5 +1,7 @@
 package com.cos.photogramstart.service;
 
+import com.cos.photogramstart.web.dto.subscribe.SubscribeDto;
+import org.qlrm.mapper.JpaResultMapper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -8,11 +10,47 @@ import com.cos.photogramstart.handler.ex.CustomApiException;
 
 import lombok.RequiredArgsConstructor;
 
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import java.util.List;
+
 @RequiredArgsConstructor
 @Service
 public class SubscribeService {
 
     private final SubscribeRepository subscribeRepository;
+    private final EntityManager em; //Repository는 EntityManager를 구현해서 만들어진 구현체임
+
+
+    @Transactional(readOnly = true)
+    public List<SubscribeDto> 구독리스트(int principalId, int pageUserId) {
+
+        //쿼리 준비
+        StringBuffer sb = new StringBuffer();
+        sb.append("SELECT u.id, u.username, u.profileImageUrl, ");
+        sb.append("if((SELECT 1 FROM subscribe WHERE fromUserId = ? AND toUserId = u.id), 1, 0) subscribeState, ");
+        sb.append("if((?=u.id), 1, 0) equalUserState ");
+        sb.append("FROM user u INNER JOIN subscribe s ");
+        sb.append("ON u.id = s.toUserId ");
+        sb.append("WHERE s.fromUserId = ?"); //끝에 세미콜론 들어오면 안됨 !!!
+
+        //1.물음표 principalId
+        //2.물음표 principalId
+        //3.물음표 pageUserId 들어와야함
+
+        //쿼리 완성
+        Query query = em.createNativeQuery(sb.toString())
+                .setParameter(1, principalId)
+                .setParameter(2, principalId)
+                .setParameter(3, pageUserId);
+
+        //쿼리 실행 (qlrm라이브러리 사용 - DTO에 DB 결과를 맵핑하기 위해서)
+        JpaResultMapper result = new JpaResultMapper();
+        List<SubscribeDto> subscribeDtos = result.list(query, SubscribeDto.class);
+
+
+        return subscribeDtos;
+    }
 
     @Transactional
     public void 구독하기(int fromUserId, int toUserId) {
--- src/main/java/com/cos/photogramstart/service/UserService.java
+++ src/main/java/com/cos/photogramstart/service/UserService.java
@@ -4,6 +4,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.cos.photogramstart.domain.subscribe.SubscribeRepository;
 import com.cos.photogramstart.domain.user.User;
 import com.cos.photogramstart.domain.user.UserRepository;
 import com.cos.photogramstart.handler.ex.CustomException;
@@ -18,6 +19,7 @@ public class UserService {
 
     private final UserRepository userRepository;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final SubscribeRepository subscribeRepository;
 
     @Transactional(readOnly = true)
     public UserProfileDto 회원프로필(int pageUserId, int principalId) {
@@ -30,6 +32,13 @@ public class UserService {
         dto.setUser(userEntity);
         dto.setPageOwnerState(pageUserId == principalId); // 1은 페이지 주인, -1은 주인이 아님
         dto.setImageCount(userEntity.getImages().size());
+
+        int subscribeState = subscribeRepository.mSubscribeState(principalId, pageUserId);
+        int subscribeCount = subscribeRepository.mSubscribeCount(pageUserId);
+
+        dto.setSubscribeCount(subscribeCount);
+        dto.setSubscribeState(subscribeState == 1);
+
         return dto;
     }
 
