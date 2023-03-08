d105b39b4adee21840f4b5a3c895ecbd61627b28
unknown <ppkk34@gmail.com>
2023-03-08 23:22:35 +0900

    0308

--- src/main/java/com/cos/photogramstart/domain/image/Image.java
+++ src/main/java/com/cos/photogramstart/domain/image/Image.java
@@ -1,15 +1,11 @@
 package com.cos.photogramstart.domain.image;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.PrePersist;
+import javax.persistence.*;
 
+import com.cos.photogramstart.domain.likes.Likes;
 import com.cos.photogramstart.domain.user.User;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
@@ -39,6 +35,8 @@ public class Image {
     private User user;
 
     // 이미지 좋아요
+    @OneToMany(mappedBy = "image")
+    private List<Likes> likes;
 
     // 댓글
 
